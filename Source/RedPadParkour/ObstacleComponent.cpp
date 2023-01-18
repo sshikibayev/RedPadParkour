e28384eca9041cae8a49b562671556e88e1ed35d
DaFaRe <89894897+sshikibayev@users.noreply.github.com>
2023-01-18 22:18:02 +0600

    working collision

--- Source/RedPadParkour/ObstacleComponent.cpp
+++ Source/RedPadParkour/ObstacleComponent.cpp
@@ -8,12 +8,37 @@ UObstacleComponent::UObstacleComponent()
 
 void UObstacleComponent::BeginPlay()
 {
-	Super::BeginPlay();	
-}
+	Super::BeginPlay();
 
+	setupCollision();
+}
 
 void UObstacleComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	UE_LOG(LogTemp, Display, TEXT("Tick inside component"));
+}
+
+void UObstacleComponent::setupCollision()
+{
+	collision_box = GetOwner()->FindComponentByClass<UBoxComponent>();
+	if (collision_box) {
+		collision_box->SetCollisionProfileName(TEXT("Trigger"));
+
+		collision_box->OnComponentBeginOverlap.AddDynamic(this, &UObstacleComponent::OnBeginOverlap);
+		collision_box->OnComponentEndOverlap.AddDynamic(this, &UObstacleComponent::OnEndOverlap);
+	}
+}
+
+void UObstacleComponent::OnBeginOverlap(UPrimitiveComponent* overlapped_component, AActor* other_actor, UPrimitiveComponent* other_component, int32 body_index, bool from_sweep, const FHitResult& hit_result)
+{
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Overlapped begin"));
+	}
+}
+
+void UObstacleComponent::OnEndOverlap(UPrimitiveComponent* overlapped_component, AActor* other_actor, UPrimitiveComponent* other_component, int32 body_index)
+{
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Overlapped end"));
+	}
 }
--- Source/RedPadParkour/ObstacleComponent.h
+++ Source/RedPadParkour/ObstacleComponent.h
@@ -2,20 +2,31 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Components/BoxComponent.h"
+
 #include "ObstacleComponent.generated.h"
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class REDPADPARKOUR_API UObstacleComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	UObstacleComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 	virtual void BeginPlay() override;
 
-public:	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* collision_box;
+
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* overlapped_component, AActor* other_actor, UPrimitiveComponent* other_component, int32 body_index, bool from_sweep, const FHitResult& hit_result);
+
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* overlapped_component, AActor* other_actor, UPrimitiveComponent* other_component, int32 body_index);
+
+	void setupCollision();
 };
